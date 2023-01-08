1d873273d9d0ba57d9a2355d9626753d14f496f9
Oskar Jaworski <oskiflog@gmail.com>
2023-01-08 13:44:42 +0100

    <Instagram/>: Add HTML structure

--- src/app/components/Instagram/Instagram.component.tsx
+++ src/app/components/Instagram/Instagram.component.tsx
@@ -1,6 +1,56 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faSquarePlus, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+
 const Instagram = () =>
 	<div className="instagram">
-		INSTAGRAM
+		<div className="instagram__header">
+			<div className="instagram__header-logo">
+			</div>
+			<div className="instagram__header-plus">
+				<FontAwesomeIcon
+					icon={ faSquarePlus }
+					color="white"
+					size="xs"
+				/>
+			</div>
+			<div className="instagram__header-liked">
+				<FontAwesomeIcon
+					icon={ faHeart }
+					color="white"
+					size="xs"
+				/>
+			</div>
+			<div className="instagram__header-dm">
+				<FontAwesomeIcon
+					icon={ faPaperPlane }
+					color="white"
+					size="xs"
+				/>
+			</div>
+		</div>
+		<div className="instagram__stories-wrapper">
+
+		</div>
+		<div className="instagram__content">
+
+		</div>
+		<div className="instagram__footer">
+			<div className="instagram__footer-home">
+
+			</div>
+			<div className="instagram__footer-search">
+
+			</div>
+			<div className="instagram__footer-reels">
+
+			</div>
+			<div className="instagram__footer-shop">
+
+			</div>
+			<div className="instagram__footer-profile">
+
+			</div>
+		</div>
 	</div>
 
 export default Instagram;
